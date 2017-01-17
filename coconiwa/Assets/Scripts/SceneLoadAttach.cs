a7842b0ce2a2b88672ec77589935c02155b5ce21
DESKTOP-219MNUS\kouki <standchat0820@gmail.com>
2017-01-17 23:44:50 +0900

    firstコミット

---
+++ coconiwa/Assets/Scripts/SceneLoadAttach.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadAttach : MonoBehaviour {
+
+    [SerializeField]
+    string LoadSceneName;
+
+    public void LoadSceneAsync()
+    {
+        try
+        {
+            SceneManager.LoadSceneAsync(LoadSceneName);
+        }
+        catch
+        {
+            Debug.Log("LoadSceneNotFound");
+        }
+    }
+
+    public void LoadScene()
+    {
+        try
+        {
+            SceneManager.LoadScene(LoadSceneName);
+        }
+        catch
+        {
+            Debug.Log("LoadSceneNotFound");
+        }
+    }
+}
