57fbce6334a997003774d463d16eaf1c428f06ab
DESKTOP-219MNUS\kouki <standchat0820@gmail.com>
2017-01-30 00:06:33 +0900

    画像の圧縮、アバウトの追加

--- coconiwa/Assets/Scripts/ContentManager.cs
+++ coconiwa/Assets/Scripts/ContentManager.cs
@@ -16,6 +16,18 @@ public class ContentManager : MonoBehaviour
 
     [SerializeField]
     Text ContentText;
+    [SerializeField]
+    Image ContentsBack;
+
+    [SerializeField]
+    Image Header;
+
+    [SerializeField]
+    Sprite Inter;
+    [SerializeField]
+    Sprite Artfact;
+    [SerializeField]
+    Sprite Prants;
 
     private int index;
 
@@ -26,6 +38,26 @@ public class ContentManager : MonoBehaviour
         m_Image.sprite = Resources.Load<Sprite>(contentsData.Elements[index].FileID);
         ContentName.text = contentsData.Elements[index].ContentsName;
         ContentText.text = contentsData.Elements[index].ContentsText;
+
+        char h= contentsData.Elements[index].FileID[0];
+
+        //タイプによって画像、色の変更
+        if (h=='A')
+        {
+            Header.sprite = Artfact;
+            ContentsBack.color = new Color(0.65f, 0.25f, 0.0f);
+        }
+        else if(h=='P')
+        {
+            Header.sprite = Prants;
+        }
+        else if(h=='I')
+        {
+            Header.sprite = Inter;
+            ContentsBack.color = new Color(0.19f, 0.3f, 0.54f);
+        }
+
+
 	}
 	
 
