7728843ad2075c25e86397275520d2e3ecc0e2cd
skuirrel0311 <rureikus7223@gmail.com>
2017-01-31 14:47:11 +0900

    【Map】マップ画面の操作性の向上

--- coconiwa/Assets/Scripts/MapManager.cs
+++ coconiwa/Assets/Scripts/MapManager.cs
@@ -9,18 +9,16 @@ public class MapManager : MonoBehaviour
     GameObject Map;
 
     //カメラ視覚の範囲
-    float viewMin = 20.0f;
-    float viewMax = 60.0f;
-
     float vMin = 1.0f;
     float vMax = 5.0f;
-
-    //直前の2点間の距離.
-    private float backDist = 0.0f;
     //初期値
-    float view = 60.0f;
     float v = 1.0f;
 
+    //最初にタッチした時の2点間の距離.
+    private float backDist = 0.0f;
+
+    //指を離した時間
+    private Timer separateTime = new Timer();
 
     // Use this for initialization
     void Start()
@@ -30,22 +28,22 @@ public class MapManager : MonoBehaviour
 
     private void OnDestroy()
     {
-       TouchManager.Instance.Drag -= OnMapSwipe;
+        TouchManager.Instance.Drag -= OnMapSwipe;
     }
 
     void OnMapSwipe(object sender, CustomInputEventArgs e)
     {
 
-        this.Map.transform.position += new Vector3(e.Input.DeltaPosition.x, e.Input.DeltaPosition.y, 0)*5.0f;
+        this.Map.transform.position += new Vector3(e.Input.DeltaPosition.x, e.Input.DeltaPosition.y, 0) * 5.0f;
 
-        float t = 1000.0f*Map.transform.localScale.x;
+        float t = 1000.0f * Map.transform.localScale.x;
         float yt = 500.0f * Map.transform.localScale.y;
 
         if (Map.transform.localPosition.x > t)
         {
             Map.transform.localPosition = new Vector3(t, Map.transform.localPosition.y, Map.transform.localPosition.z);
         }
-         if (Map.transform.localPosition.x < -t)
+        if (Map.transform.localPosition.x < -t)
         {
             Map.transform.localPosition = new Vector3(-t, Map.transform.localPosition.y, Map.transform.localPosition.z);
         }
@@ -63,40 +61,81 @@ public class MapManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount >= 2)
+        separateTime.Update();
+        if (separateTime.IsLimitTime) separateTime.Stop(true);
+
+        if (!Application.isEditor)
         {
-            // タッチしている２点を取得
-            Touch t1 = Input.GetTouch(0);
-            Touch t2 = Input.GetTouch(1);
-
-            //2点タッチ開始時の距離を記憶
-            if (t2.phase == TouchPhase.Began)
-            {
-                backDist = Vector2.Distance(t1.position, t2.position);
-            }
-            else if (t1.phase == TouchPhase.Moved && t2.phase == TouchPhase.Moved)
-            {
-                // タッチ位置の移動後、長さを再測し、前回の距離からの相対値を取る。
-                float newDist = Vector2.Distance(t1.position, t2.position);
-                view = view + (backDist - newDist) / 100.0f;
-                v = v + (newDist - backDist) / 1000.0f;
-
-                // 限界値をオーバーした際の処理
-                if (v > vMax)
-                {
-                    v = vMax;
-                }
-                else if (v < vMin)
-                {
-                    v = vMin;
-                }
-
-                // 相対値が変更した場合、カメラに相対値を反映させる
-                if (v != 0)
-                {
-                    Map.transform.localScale = new Vector3(v, v, 1.0f);
-                }
-            }
+            v += GetPinchValue();
         }
+        else
+        {
+            v += Input.mouseScrollDelta.y * 0.2f;
+        }
+        
+        // 限界値をオーバーした際の処理
+        if (v > vMax)
+        {
+            if(Input.touchCount <= 0)
+            {
+                if(!separateTime.IsWorking) separateTime.TimerStart(1.0f);
+                v = FloatLerp(v, vMax, separateTime.Progress);
+            }
+
+            v = Mathf.Min(v, vMax + 3.0f);
+        }
+        else if (v < vMin)
+        {
+            if (Input.touchCount <= 0)
+            {
+                if (!separateTime.IsWorking) separateTime.TimerStart(1.0f);
+                v = FloatLerp(v, vMin, separateTime.Progress);
+            }
+
+            v = Mathf.Max(v, vMin * 0.5f);
+        }
+
+        // 相対値が変更した場合、カメラに相対値を反映させる
+        if (v != 0)
+        {
+            Map.transform.localScale = new Vector3(v, v, 1.0f);
+        }
+    }
+
+    //２本の指の感覚を広くした時がプラス、狭めた時がマイナスの値になる
+    float GetPinchValue()
+    {
+        if (Input.touchCount < 2) return 0.0f;
+
+        // タッチしている２点を取得
+        Touch t1 = Input.GetTouch(0);
+        Touch t2 = Input.GetTouch(1);
+
+        //2点タッチ開始時の距離を記憶
+        if (t2.phase == TouchPhase.Began)
+        {
+            backDist = Vector2.Distance(t1.position, t2.position);
+            return 0.0f;
+        }
+        else if (t1.phase == TouchPhase.Moved || t2.phase == TouchPhase.Moved)
+        {
+            float speed = 0.0001f;
+            if(t1.phase == TouchPhase.Stationary)
+            {
+                speed = 0.0002f;
+            }
+
+            // タッチ位置の移動後、長さを再測し、前回の距離からの相対値を取る。
+            return (Vector2.Distance(t1.position, t2.position) - backDist) * speed;
+        }
+
+        return 0.0f;
+    }
+
+    public float FloatLerp(float a, float b, float t)
+    {
+        t = Mathf.Clamp(t, 0, 1);
+
+        return a + ((b - a) * t);
     }
 }
