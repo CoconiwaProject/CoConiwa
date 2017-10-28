29fb2347e99c5851efba4d3bd2ef80adf495b0c0
Kazuki <k.kaneko0311@gmail.com>
2017-10-28 20:12:11 +0900

    コンテンツ調整。

--- coconiwa/Assets/Scripts/ContentsTextController.cs
+++ coconiwa/Assets/Scripts/ContentsTextController.cs
@@ -6,8 +6,6 @@ using UnityEngine.EventSystems;
 
 public class ContentsTextController : MonoBehaviour
 {
-
-
     [SerializeField]
     RectTransform[] setTextTransforms = null;
 
@@ -23,38 +21,24 @@ public class ContentsTextController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        //titleInterval = new Vector2(setTextTransforms[0].position.x-contentsText.gameObject.transform.position.x,
-        //    setTextTransforms[0].position.y - contentsText.gameObject.transform.position.y);
-        //imageInterval = new Vector2(setTextTransforms[1].position.x - contentsText.gameObject.transform.position.x,
-        //    setTextTransforms[1].position.y - contentsText.gameObject.transform.position.y);
-
-        float boxSize = 0;
-        foreach (RectTransform c in setTextTransforms)
+        //SizeFilterの変更に１フレームかかるのでディレイをかける
+        KKUtilities.WaitSeconde(0.01f, () =>
         {
-            boxSize += c.sizeDelta.y;
-        }
+            Vector2 tmp = Vector2.down * (setTextTransforms[0].sizeDelta.y - 86.0f);
+            setTextTransforms[1].anchoredPosition += tmp;
+            setTextTransforms[2].anchoredPosition += tmp;
 
-        Vector2 tmp = Vector2.down * (setTextTransforms[0].sizeDelta.y);
-        Debug.Log(tmp.y);
-
-        setTextTransforms[1].anchoredPosition -= tmp;
-        setTextTransforms[2].anchoredPosition -= tmp;
-
-
-
-        ContentsBox.sizeDelta = new Vector2(ContentsBox.sizeDelta.x, boxSize);
+            float boxSize = 0.0f;
+            foreach (RectTransform c in setTextTransforms)
+            {
+                boxSize += c.sizeDelta.y;
+            }
+            ContentsBox.sizeDelta = new Vector2(ContentsBox.sizeDelta.x, boxSize);
+        }, this);
     }
 
     public void SetTextInterval()
     {
         contentsText.text = "\n\n" + contentsText.text;
     }
-
-    // Update is called once per frame
-    //void Update () {
-    //    setTextTransforms[0].transform.position = new Vector3(contentsText.gameObject.transform.position.x + titleInterval.x, 
-    //        contentsText.gameObject.transform.position.y + titleInterval.y, 0);
-    //    setTextTransforms[1].transform.position = new Vector3(contentsText.gameObject.transform.position.x + imageInterval.x, 
-    //        contentsText.gameObject.transform.position.y + imageInterval.y, 0);
-    //}
 }
