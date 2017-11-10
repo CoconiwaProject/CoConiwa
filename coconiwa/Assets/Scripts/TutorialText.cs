6ec5a6ed0ed0d27d4d7366bd41eeb79d1080eb37
mikado <g1527066@st.t-kougei.ac.jp>
2017-11-10 22:09:53 +0900

    チュートリアル調整中

---
+++ coconiwa/Assets/Scripts/TutorialText.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialText : MonoBehaviour {
+
+    [SerializeField]
+     string[] Japanese  = null;
+    [SerializeField]
+    string[] English = null;
+    [SerializeField]
+    string[] Korea = null;
+    [SerializeField]
+    string[] ChinaSimplification = null;
+    [SerializeField]
+    string[] ChinaTraditional = null;
+
+    [SerializeField]
+    List<Text> texts = null;
+
+    // Use this for initialization
+    void Start () {
+		switch(AppData.UsedLanguage)
+        {
+            case SystemLanguage.Japanese:
+                SetText(Japanese);
+                break;
+            case SystemLanguage.Korean:
+                SetText(Korea);
+                break;
+            case SystemLanguage.Chinese:
+                SetText(ChinaSimplification);
+                break;
+            case SystemLanguage.ChineseSimplified:
+                SetText(ChinaSimplification);
+                break;
+            case SystemLanguage.ChineseTraditional:
+                SetText(ChinaTraditional);
+                break;
+            default:
+                SetText(Japanese);
+                break;
+        }
+    }
+
+
+    private void SetText(string[] setTexts)
+    {
+        for (int i = 0; i < setTexts.Length; i++)
+        {
+            texts[i].text = setTexts[i];
+        }
+    }	
+}
