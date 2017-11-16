0b98a315b439f89eab7020bb329dcd97bd5ddc34
skuirrel0311 <rureikus7223@gmail.com>
2017-11-17 03:10:45 +0900

    コンテンツの画像複数枚に対応中

--- coconiwa/Assets/Scripts/ContentManager.cs
+++ coconiwa/Assets/Scripts/ContentManager.cs
@@ -8,70 +8,97 @@ public class ContentManager : MonoBehaviour
     ContentsData contentsData;
 
     [SerializeField]
-    Image m_Image;
+    Transform imageContainer = null;
+
+    const int maxImageNum = 3;
+    Image[] images = null;
+
+    public bool isMulti = false;
 
     [SerializeField]
-    Text ContentName;
+    Text ContentName = null;
 
     [SerializeField]
-    HyphenationJpn ContentText;
+    HyphenationJpn ContentText = null;
     [SerializeField]
-    Image ContentsBack;
+    Image ContentsBack = null;
 
     [SerializeField]
-    Image Header;
+    Image Header = null;
 
     [SerializeField]
-    Sprite Inter;
+    Sprite Inter = null;
     [SerializeField]
-    Sprite Artfact;
+    Sprite Artfact = null;
     [SerializeField]
-    Sprite Prants;
+    Sprite Prants = null;
 
     [SerializeField]
-    ContentsTextController contentsTextController;
+    ContentsTextController contentsTextController = null;
 
     private int index;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         contentsData = AppData.ContentsData;
         index = GetIndex(AppData.SelectTargetName);
-     
+
         if (index == -1) return;
-        m_Image.sprite = Resources.Load<Sprite>(contentsData.Elements[index].FileID);
-        ContentName.text=contentsData.Elements[index].ContentsName;
-       ContentText.GetText(contentsData.Elements[index].ContentsText);
-       
+
+        string fileName = "";
+        char c = 'b';
+
+        images = new Image[maxImageNum];
+        for (int i = 0; i < maxImageNum; i++)
+        {
+            images[i] = imageContainer.GetChild(i).GetComponent<Image>();
+            fileName = contentsData.Elements[index].FileID;
+
+            if (i > 0)
+            {
+                fileName = contentsData.Elements[index].FileID + c;
+                c++;
+            }
+
+            Debug.Log(fileName);
+            images[i].sprite = Resources.Load<Sprite>(fileName);
+            if (images[i].sprite == null) images[i].gameObject.SetActive(false);
+            else isMulti = true;
+        }
+
+        
+        ContentName.text = contentsData.Elements[index].ContentsName;
+        ContentText.GetText(contentsData.Elements[index].ContentsText);
+
         contentsTextController.SetTextInterval();
-        char h= contentsData.Elements[index].FileID[0];
+        char h = contentsData.Elements[index].FileID[0];
 
         //タイプによって画像、色の変更
-        if (h=='A')
+        if (h == 'A')
         {
             Header.sprite = Artfact;
             ContentsBack.color = new Color(0.65f, 0.25f, 0.0f);
         }
-        else if(h=='P')
+        else if (h == 'P')
         {
             Header.sprite = Prants;
         }
-        else if(h=='I')
+        else if (h == 'I')
         {
             Header.sprite = Inter;
             ContentsBack.color = new Color(0.19f, 0.3f, 0.54f);
         }
 
 
-	}
-	
+    }
 
-   int GetIndex(string name)
+
+    int GetIndex(string name)
     {
-        for(int i=0;i<contentsData.Elements.Count;i++)
+        for (int i = 0; i < contentsData.Elements.Count; i++)
         {
-            if(contentsData.Elements[i].FileID==name)
+            if (contentsData.Elements[i].FileID == name)
             {
                 return i;
             }
---
+++ coconiwa/Assets/Scripts/ContentsSwipeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContentsSwipeController : SingletonMonoBehaviour<ContentsSwipeController>
+{
+    public void SetImageNum(int imageNum)
+    {
+        //イメージの数が１枚以下ならスワイプする必要はない
+        if (imageNum <= 1) return;
+
+        //todo : イメージの数に合わせて丸ポチを配置する
+    }
+}
